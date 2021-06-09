b072b3a3d7e078388a9382ec59f38bf363e8ce20
eilla1 <git@ella.cx>
2021-06-09 00:47:26 -0700

    add ninja dino

--- pages/api/set-pfp.ts
+++ pages/api/set-pfp.ts
@@ -7,21 +7,23 @@ const images = {
     "https://cloud-hvd4ca9hs-hack-club-bot.vercel.app/0image_from_ios.png",
   uwucat:
     "https://cdn.discordapp.com/attachments/829562865215471627/852085432245682256/pfp_uwucat.png",
+  ninjadino:
+    "https://cdn.discordapp.com/attachments/829562865215471627/852091148292849744/ninjadinopfp.png",
 };
 
 async function setPFP() {
   const hour = new Date().getHours() - 8;
   let image: any;
   if (5 < hour && hour < 12) {
-    image = await axios.get(images.normal, {
-      responseType: "arraybuffer",
-    });
-  } else if (12 < hour && hour < 15) {
     image = await axios.get(images.uwucat, {
       responseType: "arraybuffer",
     });
-  } else if (15 < hour && hour < 17) {
+  } else if (12 < hour && hour < 15) {
     image = await axios.get(images.normal, {
+      responseType: "arraybuffer",
+    });
+  } else if (15 < hour && hour < 17) {
+    image = await axios.get(images.ninjadino, {
       responseType: "arraybuffer",
     });
   } else if (17 < hour && hour < 20) {
@@ -29,7 +31,7 @@ async function setPFP() {
       responseType: "arraybuffer",
     });
   } else {
-    image = await axios.get(images.normal, {
+    image = await axios.get(images.ninjadino, {
       responseType: "arraybuffer",
     });
   }
