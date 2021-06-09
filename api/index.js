fe3ba9efe0d7b2ea5e136e85432d3aa084aea302
Ella <72365100+eilla1@users.noreply.github.com>
2021-06-08 20:13:59 -0700

    change to eleeza pop

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ const { WebClient } = require('@slack/web-api');
 const axios = require('axios').default;
 
 const images = {
-  "morning": "https://images.unsplash.com/photo-1490650034439-fd184c3c86a5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxNDk0OTAwfHxlbnwwfHx8&w=1000&q=80",
-  "afternoon": "https://images.unsplash.com/photo-1561389881-dac6bb97f175?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bG92ZWx5JTIwY2F0fGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80",
-  "night": "https://images.unsplash.com/photo-1570534536531-c3def02ad855?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMGNhdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-  "moreafternoon": "https://images.unsplash.com/photo-1557743952-b088259408a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-  "evening": "https://images.unsplash.com/photo-1539562151676-2cce8ca47d04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+  "morning": "https://cloud-hvd4ca9hs-hack-club-bot.vercel.app/0image_from_ios.png",
+  "afternoon": "https://cloud-hvd4ca9hs-hack-club-bot.vercel.app/0image_from_ios.png",
+  "night": "https://cloud-hvd4ca9hs-hack-club-bot.vercel.app/0image_from_ios.png",
+  "moreafternoon": "https://cloud-hvd4ca9hs-hack-club-bot.vercel.app/0image_from_ios.png",
+  "evening": "https://cloud-hvd4ca9hs-hack-club-bot.vercel.app/0image_from_ios.png"
 }
 
 async function setPFP() {
